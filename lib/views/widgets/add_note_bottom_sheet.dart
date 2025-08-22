3e4c8b6b4e4dfc2b2eb41712e8b8be63bbac870b
islam kamal <islam.kamal.fci@gmail.com>
2025-08-22 15:44:15 +0300

    note view ui done

---
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/note_item.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 import 'custom_app_bar.dart';
+
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -13,8 +15,13 @@ class NotesViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          CustomAppBar(),
-          Expanded(child: NotesListView()),
+          CustomAppBar(
+            title: 'Notes',
+            icon: Icons.search,
+          ),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
