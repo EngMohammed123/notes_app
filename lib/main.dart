88667fc756d7aebd88366f6e7addb2e306c0fdbd
islam kamal <islam.kamal.fci@gmail.com>
2025-08-22 14:37:27 +0300

    create notes list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+      theme: ThemeData(brightness: Brightness.dark ,
+          fontFamily: 'Poppins'
+      ),      home: const NotesView(),
     );
   }
 }
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:notes_app/views/widgets/notes_view_body.dart';
+class NotesView extends StatelessWidget {
+  const NotesView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
+  }
+}
