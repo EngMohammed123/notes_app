93ad0b134623e9ef0c10642dba24b3ab1cd97a7e
islam kamal <islam.kamal.fci@gmail.com>
2025-08-27 00:00:15 +0300

    edit note done

--- lib/views/widgets/custom_icon.dart
+++ lib/views/widgets/custom_icon.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomIcon extends StatelessWidget {
-  const CustomIcon({Key? key, required this.icon}) : super(key: key);
+  const CustomIcon({Key? key, required this.icon, this.onPressed}) : super(key: key);
   final IconData icon;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class CustomIcon extends StatelessWidget {
           color: Colors.white.withOpacity(.05),
           borderRadius: BorderRadius.circular(16)),
       child: IconButton(
-        onPressed: () {},
+        onPressed: onPressed,
         icon: Icon(
           icon,
           size: 28,
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -4,18 +4,19 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1, this.onSaved});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1, this.onSaved, this.onChanged});
 
   final String hint;
-
   final int maxLines;
 
   final void Function(String?)? onSaved;
+  final void Function(String)? onChanged;
 
 @override
   Widget build(BuildContext context) {
     return TextFormField(
       onSaved: onSaved,
+      onChanged: onChanged,
       validator: (value){
         if(value?.isEmpty ?? true){
           return 'Field is required';
